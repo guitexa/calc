c76b3289bd2358bc47bcecfead4137cee5f91425
Guilherme Teixeira <guilhermetexa@outlook.com>
2019-08-07 23:57:46 -0300

    ESLint fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,20 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
-devServer: {
-  contentBase: path.resolve(__dirname, 'public'),
-},
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
-}
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
